33b28a376da77dbfd6a79db03535ad49e41d2ad7
soumojitjati51-creator <soumojitjati51@gmail.com>
2026-05-22 11:03:54 +0530

    added App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,51 @@
-import Navbar from "./components/Navbar"
+// import Navbar from "./components/Navbar";
+// import Hero from "./components/Hero";
+// import About from "./components/About";
+// import Features from "./components/Features";
+// import Login from "./components/Login";
+
+// function App() {
+//   return (
+
+//     <div className="min-h-screen bg-slate-900 p-6">
+
+//       <Navbar />
+//       <Hero/>
+//       <About />
+//       <Features/>
+//       <Login/>
+//     </div>
+
+//   )
+// }
+
+// export default App
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
+
   return (
 
-    <div className="min-h-screen bg-slate-900 p-6">
+    <BrowserRouter>
 
-      <Navbar />
+      <Routes>
 
-    </div>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/login" element={<Login />} />
+
+        <Route path="/signup" element={<Signup />} />
+
+        <Route path="/dashboard" element={<Dashboard />} />
+
+      </Routes>
+
+    </BrowserRouter>
 
   )
 }
